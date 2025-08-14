ddac3958db6c2eb6423f7e2b03940596fe420966
sohila558 <sohailat.441@gmail.com>
2025-08-14 16:14:15 +0300

    first commit

---
+++ AssignmentADV04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentADV04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
